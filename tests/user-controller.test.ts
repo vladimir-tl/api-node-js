b945cacfc597af644748b5819af7c1f4f60c3656
vladimir-tl <151621590+vladimir-tl@users.noreply.github.com>
2024-11-04 16:53:19 +0200

    Fix/docker compose (#2)

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -4,7 +4,7 @@ let baseURL: string = 'http://localhost:3000/users';
 
 test.describe('User management API', () => {
 
-    test('GET / - should return all users', async ({ request }) => {
+    test('GET / - should return empty when no users', async ({ request }) => {
         const response = await request.get(`${baseURL}`);
         expect(response.status()).toBe(200);
         const responseBody = await response.text()
